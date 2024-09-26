275f9bd57a216e9a4dd67b4d2ce6f96b4cfbfb91
Dodi55 <sasha.derevyanko.2018@mail.ru>
2024-09-26 20:30:10 +0500

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import { useRef } from 'react';
+import './App.sass'
+// https://maxmobiles.ru/images/logos/73/logo__.svg
+
+function App() {
+    
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
